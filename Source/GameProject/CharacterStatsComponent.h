e1e9c76b1bf054e3cad70b1ba55de1ca3f2ba6cd
DESKTOP-1HOV35S\aljon <>
2021-01-29 22:00:39 +0800

    added heal function

--- Source/GameProject/CharacterStatsComponent.h
+++ Source/GameProject/CharacterStatsComponent.h
@@ -26,6 +26,7 @@ protected:
 	UPROPERTY(BlueprintReadOnly) float charMaxHp;
 	UPROPERTY(BlueprintReadWrite) UBoxComponent* hitBox;
 	UPROPERTY(BlueprintReadWrite) float weaponDamage;
+	UFUNCTION(BlueprintCallable) void HealCharacter(float healAmount);
 	UFUNCTION()
 	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 	UFUNCTION()
